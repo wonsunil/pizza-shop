dc3edc0143bec336b9832dbf8cad8fe35b6dcd53
wonsunil <seonil6352@gmail.com>
2020-12-10 13:21:01 +0900

    Feat: db connection file

---
+++ pizza-shop/src/DB.java
@@ -0,0 +1,55 @@
+
+import java.sql.*;
+
+public class DB {
+	public static Connection getDB() throws Exception {
+		Class.forName("oracle.jdbc.OracleDriver");
+        
+        return DriverManager.getConnection("jdbc:oracle:thin:@//localhost/xe", "pizza_shop", "1234");
+	};
+	public static boolean execute(String query, String[] datas) {
+		Connection db = null;
+		
+		try {
+			db = getDB();
+			
+			PreparedStatement pstmt = db.prepareStatement(query);
+			
+			int index = 1;
+			for(String data : datas) {
+				pstmt.setString(index, data);
+				
+				index++;
+			}
+			
+			return pstmt.execute();
+		} catch (Exception ignored) {
+			return false;
+		} finally {
+			try {
+				db.close();
+			} catch (SQLException ignored) {}
+		}
+	};
+	public static ResultSet fetch(String query) throws Exception {
+		Connection db = getDB();
+		ResultSet rs = db.prepareStatement(query).executeQuery();
+		
+		return rs;
+	};
+	public static ResultSet fetch(String query, String[] datas) throws Exception {
+		Connection db = getDB();
+		PreparedStatement pstmt = db.prepareStatement(query);
+		
+		int index = 1;
+		for(String data : datas) {
+			pstmt.setString(index, data);
+			
+			index++;
+		}
+		
+		ResultSet rs = pstmt.executeQuery();
+		
+		return rs;
+	};
+}
